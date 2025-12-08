aff947fcc11b4b6abcb9d430fe1c4c4b9c485e52
JuGo-CS <mondejarkenneth64@gmail.com>
2025-12-08 14:28:18 +0800

    finish game introduction and tutorial

--- brew_or_boom.py
+++ brew_or_boom.py
@@ -1,13 +1,68 @@
 from potions import Potions
 from lab8 import HashTableLP
 import game_aesthetics
+import time
+import os
+
+BOLD = '\033[1m'
+FAINT = '\033[2m'
+ITALIC = '\033[3m' 
+CYAN = '\033[36m'
+RED = '\033[31m'
+RESET = '\033[0m'
+UNDERLINED = '\033[4m'
 
 
-# game_aesthetics.game_start()
+action_list = f"""{BOLD}
+    ╦ ╦┌─┐┬ ┬┬─┐  ╔═╗┌─┐┌┬┐┬┌─┐┌┐┌
+    ╚╦╝│ ││ │├┬┘  ╠═╣│   │ ││ ││││o
+     ╩ └─┘└─┘┴└─  ╩ ╩└─┘ ┴ ┴└─┘┘└┘o
+        [1] Potion's Book
+        [2] Brew Potion
+        [3] Give it to Customer{RESET}
+    
+"""
+
+brew_potion_list = f"""{BOLD}
+    ╦ ╦┌─┐┬ ┬┬─┐  ╔╦╗┌─┐┬  ┬┌─┐
+    ╚╦╝│ ││ │├┬┘  ║║║│ │└┐┌┘├┤ o
+     ╩ └─┘└─┘┴└─  ╩ ╩└─┘ └┘ └─┘o
+        [1] Add Ingredient
+        [2] Undo Move
+        [3] View Potion's Details
+        [4] Finish Brewing{RESET}
+        
+"""
+
+def player_action():
+    while(True):
+        print(action_list)
+        players_input = int(input(f"{CYAN}Input their correspoding value: {RESET}"))
+
+        if players_input >= 1 and players_input <= 3:
+            break
+    
+    return players_input
+
+def player_brew_action():
+    while(True):
+        print(brew_potion_list)
+        players_brew_input = int(input(f"{CYAN}Input their correspoding value: {RESET}"))
+
+        if players_brew_input >= 1 and players_brew_input <= 4:
+            break
+    
+    return players_brew_input
+
+
+
 potion_instance = Potions()
-brew_catalog = potion_instance.potion_recipe()
+potion_catalog = potion_instance.potion_recipe()
 
-print(brew_catalog)
+game_aesthetics.game_start()
+
+
+
 
 
 
--- game_aesthetics.py
+++ game_aesthetics.py
@@ -8,6 +8,7 @@ ITALIC = '\033[3m'
 CYAN = '\033[36m'
 RED = '\033[31m'
 RESET = '\033[0m'
+UNDERLINED = '\033[4m'
 
 game_title = r"""
 
@@ -18,8 +19,7 @@ $$$$$$$\ |$$  __$$\ $$  __$$\ $$ | $$ | $$ |      $$  __$$\ $$  __$$\       $$$$
 $$  __$$\ $$ |  \__|$$$$$$$$ |$$ | $$ | $$ |      $$ /  $$ |$$ |  \__|      $$  __$$\ $$ /  $$ |$$ /  $$ |$$ / $$ / $$ |
 $$ |  $$ |$$ |      $$   ____|$$ | $$ | $$ |      $$ |  $$ |$$ |            $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |
 $$$$$$$  |$$ |      \$$$$$$$\ \$$$$$\$$$$  |      \$$$$$$  |$$ |            $$$$$$$  |\$$$$$$  |\$$$$$$  |$$ | $$ | $$ |
-\_______/ \__|       \_______| \_____\____/        \______/ \__|            \_______/  \______/  \______/ \__| \__| \__|
-                                                                                                                                                                                                                           
+\_______/ \__|       \_______| \_____\____/        \______/ \__|            \_______/  \______/  \______/ \__| \__| \__|                                                                                                                                                                                                                    
 """
 
 game_witch = r"""    
@@ -93,7 +93,7 @@ game_intro_story = f"""
                 {FAINT}And just like that, you became the new keeper of the potion shop. {RESET}
 
                 Now it’s your turn to brew the potions Apitong depends on.
-                With only four basic ingredients, {ITALIC} Earth, Water, Fire, and Air,{RESET} and your mother’s old 
+                With only four basic ingredients, {ITALIC}Earth, Water, Fire, and Air,{RESET} and your mother’s old 
                 recipe book, you must keep the shop running.
 
                 Customers will come. Requests will grow.
@@ -105,6 +105,113 @@ game_intro_story = f"""
 
 """
 
+tips = f"""
+                {RED}Please remember:{RESET}
+                    {BOLD}Your safety{BOLD} always comes first.
+                    Some customers might be impatient, but it’s okay if you not ready yet. 
+                    A wrong brew can be dangerous… and very explosive. It will {RED}{ITALIC}kill{RESET} you.
+
+                    Always follow each recipe {ITALIC}{CYAN}{UNDERLINED}STEP BY STEP{RESET}.
+                    Your mother left you her old Potion Book, filled with the proper ingredient
+                    orders. Whenever you feel unsure, open the book and check the recipes carefully.
+
+                    Take your time.
+                    Brew wisely.
+                    And trust yourself.
+                    Every great witch started somewhere. ⭐
+
+                    {FAINT}Good luck, young potion keeper.{RESET}
+    """
+
+game_guide_customer = f"""
+    {CYAN}“Hello there! I would like to buy a Potion of Invisibility, please!”{RESET}
+"""
+
+game_guide_starting = f"""
+    {ITALIC}This is your {BOLD}first customer.{RESET}
+    To help them, you need to brew the potion they requested."
+    Remember: in Apitong’s Potion Shop, the order of ingredients is very 
+    important. One wrong step… and things might {RED}explode and kill you{RESET}.",
+
+    Let’s take things slowly, yes? Ready? Please read the guide below.
+
+
+"""
+
+game_guide = f"""
+    ╔═╗  ╔═╗  ╔╦╗  ╔═╗      ╔═╗  ╦ ╦  ╦  ╔╦╗  ╔═╗
+    ║ ╦  ╠═╣  ║║║  ║╣       ║ ╦  ║ ║  ║   ║║  ║╣ 
+    ╚═╝  ╩ ╩  ╩ ╩  ╚═╝      ╚═╝  ╚═╝  ╩  ═╩╝  ╚═╝
+    ---------------------------------------------------------------------------------
+
+    When a customer asks for a potion, your first step is to check the Potion’s Book.
+    You'll see these options:
+        {ITALIC}{FAINT}
+        ╦ ╦┌─┐┬ ┬┬─┐  ╔═╗┌─┐┌┬┐┬┌─┐┌┐┌
+        ╚╦╝│ ││ │├┬┘  ╠═╣│   │ ││ ││││o
+         ╩ └─┘└─┘┴└─  ╩ ╩└─┘ ┴ ┴└─┘┘└┘o
+            [1] Potion's Book
+            [2] Brew Potion
+            [3] Give it to Customer {RESET}
+    
+            
+    For now, let's focus on Potion’s Book.
+
+    In the {BOLD}"Potion’s Book{RESET}, look for the potion the customer wants.
+    Here, they want Invisibility, so find the recipe for it and study 
+    the ingredients in {BOLD}{UNDERLINED}EXACT ORDER.{RESET}
+
+    After checking the ingredients, go back to the menu and choose Brew Potion.
+
+    Inside Brew Potion, you will find these options:
+        {ITALIC}{FAINT}
+        ╦ ╦┌─┐┬ ┬┬─┐  ╔╦╗┌─┐┬  ┬┌─┐
+        ╚╦╝│ ││ │├┬┘  ║║║│ │└┐┌┘├┤ o
+         ╩ └─┘└─┘┴└─  ╩ ╩└─┘ └┘ └─┘o
+            [1] Add Ingredient
+            [2] Undo Move
+            [3] View Potion's Details
+            [4] Finish Brewing {RESET}
+ 
+            
+    Here’s what each one does:
+
+        ⭐ {BOLD}{UNDERLINED}Add Ingredients{RESET}
+        Lets you add an ingredient to the cauldron.
+        Since we’re making Invisibility, choose Add, 
+        then pick the correct ingredient. One step at a time.
+
+        ----------------------------------------------------------------
+
+        ⭐ {BOLD}{UNDERLINED}Undo Move{RESET}
+        If you misclicked or added the wrong ingredient, don’t panic.
+        Choose Undo to remove the last ingredient you added.
+
+        ----------------------------------------------------------------
+
+        ⭐ {BOLD}{UNDERLINED}View Potion's Details{RESET}
+        If you forget the steps, you can always choose View to check the 
+        ingredients again. Just type the potion’s name.
+
+            {ITALIC}For example:
+                View: Invisibility{RESET}
+
+        It will show the ingredient list and its description.
+        Important: Only type the main name of the potion.
+        If the potion is {BOLD}Potion of Healing{RESET}, type only: {BOLD}Healing{RESET}
+
+        ----------------------------------------------------------------
+
+        ⭐ {BOLD}{UNDERLINED}Finish Brewing{RESET}
+        Choose this once you have added all ingredients correctly.
+        This will give the finished potion to the customer.
+
+        If the customer only wants one potion, you may now choose 
+        Finish to end the transaction.
+
+        If they want multiple potions, simply repeat the same steps 
+        for each one.
+"""
 
 
 """
@@ -151,8 +258,41 @@ def game_start():
     animate_string(game_title, type_of_animation = 0, delay = 0.4, blinking = 3)
     print(game_witch)
     time.sleep(3)
+
     os.system('cls')
     print(game_title)
     print(breaker)
     animate_string(game_intro_story, 1, 0.03)
     print(breaker)
+    time.sleep(3)
+
+    os.system('cls')
+    print(game_title)
+    print(breaker)
+    print(tips)
+    print(breaker)
+
+    time.sleep(8)
+    os.system('cls')
+    print(game_title)
+    print(breaker + "\n")
+    animate_string(game_guide_customer, type_of_animation = 1, delay = 0.03)
+    time.sleep(2)
+    animate_string(game_guide_starting, type_of_animation = 1, delay = 0.03)
+    time.sleep(2)
+
+    print(game_guide)
+
+def game_proper():
+    print(game_title)
+    print(game_guide)
+    while(True):
+        player_input = input("Proceeds to Game[Y/y]: ")
+
+        if(player_input == 'Y' or player_input == 'y'):
+            break
+
+        else:
+            print(f"{FAINT}{ITALIC}     I don't quite get it!{RESET}")
+    
+    return True
